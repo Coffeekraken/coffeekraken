150fc9e1279df6b20e5e68f94d1909c1c321e7a1
Olivier Bossel <olivier.bossel@gmail.com>
2019-12-31 15:43:07 +0100

    up

--- layout/starter-kit/.scripts/sass.js
+++ layout/starter-kit/.scripts/sass.js
@@ -1,16 +1,43 @@
 const sass = require('sass');
 const globImporter = require('sass-glob-importer');
 const fs = require('fs');
+const path = require('path');
+const Bundler = require("scss-bundle").Bundler;
 
-sass.render({
-  file: './src/scss/style.scss',
-  outputStyle: 'compressed',
-  importer: globImporter()
-}, (error, result) => {
-    if(!error){
-      // No errors during the compilation, write this result on the disk
-      fs.writeFile('./dist/css/style.css', result.css, function(err){
+(async () => {
+    // Absolute project directory path.
+    const projectDirectory = path.resolve(__dirname, "./../");
+    const bundler = new Bundler(undefined, projectDirectory);
+    // Relative file path to project directory path.
+    const result = await bundler.bundle("./src/scss/style.scss");
 
-      });
-    }
-});
+    fs.writeFileSync('.scripts/bundledcss.scss', result.bundledContent);
+
+    fs.appendFileSync('.scripts/bundledcss.scss', `
+      /**
+       * Views
+       * Import all the scss files found in the views folder
+       */
+      @import '../views/**/*.scss';
+    `);
+
+    sass.render({
+      file: `${__dirname}/bundledcss.scss`,
+      outputStyle: 'compressed',
+      includePaths: ['node_modules'],
+      importer: globImporter()
+    }, (error, result) => {
+        if(!error){
+
+          try {
+            fs.unlinkSync('./dist/css/style.css');
+          } catch(e) {}
+
+          // No errors during the compilation, write this result on the disk
+          fs.writeFileSync('./dist/css/style.css', result.css.toString());
+        } else {
+          console.log(error);
+        }
+    });
+
+})();

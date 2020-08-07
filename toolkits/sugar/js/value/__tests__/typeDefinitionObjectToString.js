75567e405b6a4538d5c2af4ea436933231a537cd
Olivier Bossel <olivier.bossel@gmail.com>
2020-08-07 18:52:57 +0200

    up

--- toolkits/sugar/js/value/__tests__/typeDefinitionObjectToString.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = __typeDefinitionObjectToString => {
-  describe('sugar.js.value.typeDefinitionObjectToString', () => {
-    it('Should return the correct typeDefinitionObjectToString of the passed values', () => {
-      expect(__typeDefinitionObjectToString(true)).toEqual('Boolean');
-    });
-  });
-};

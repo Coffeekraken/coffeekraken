4feaa249df381fecab42c459293c112b5120fbb6
Olivier Bossel <olivier.bossel@gmail.com>
2022-11-30 17:11:13 +0100

    build 01-basic

---
+++ packages/@examples/01-basic/dist/js/packageTmpDir.46a3af09.js
@@ -0,0 +1,7 @@
+import { S as SSugarConfig } from "./index.caaeb84d.js";
+function __packageTmpDir() {
+  return SSugarConfig.get("storage.package.tmpDir");
+}
+export {
+  __packageTmpDir as default
+};

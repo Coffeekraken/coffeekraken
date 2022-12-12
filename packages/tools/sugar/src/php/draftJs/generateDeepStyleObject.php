39050cc7b6e3f7d8a6f3abd057b0439c9cab800b
Olivier Bossel <olivier.bossel@gmail.com>
2022-12-12 17:52:03 +0100

    add draftJs php functions

---
+++ packages/tools/sugar/src/php/draftJs/generateDeepStyleObject.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Sugar\draftJs;
+
+/**
+ * @name            generateDeepStyleObject
+ * @namespace       php.draftJs
+ * @type            Function
+ * @platform        php
+ * @status          beta
+ *
+ * Create deep object used to render DraftJs HTML
+ *
+ * @param       {Array}         $block                      DraftJs JSON object
+ * @param       {String}        $style                      Typo to render
+ * @param       {Integer}       $char_position              Position in content treatment - Start to 0
+ * @param       {Integer}       $keyInlineStylePosition     Index of current InlineStyle treatment
+ * @param       {Array}         $breakPoint                 Contains deep elements to close
+ * @return      {Array}         Deep object
+ *
+ * @since       2.0.0
+ * @author    Paul Balanche <pb@buzzbrothers.ch> (https://buzzbrothers.ch)
+ */
+function generateDeepStyleObject(
+    $block,
+    $style = null,
+    &$char_position = 0,
+    &$keyInlineStylePosition = -1,
+    &$breakPoint = []
+) {
+    $content = [''];
+
+    $textSplitted = str_split($block['text']);
+    foreach ($textSplitted as $key => $character) {
+        if ($key < $char_position) {
+            continue;
+        }
+
+        if (isset($breakPoint[$key]) && $breakPoint[$key] > 0) {
+            $breakPoint[$key]--;
+            break;
+        }
+
+        // Opening sub-style
+        foreach (
+            $block['inlineStyleRanges']
+            as $keyInlineStyle => $inlineStyle
+        ) {
+            if (
+                $inlineStyle['offset'] == $key &&
+                $keyInlineStyle > $keyInlineStylePosition
+            ) {
+                $keyInlineStylePosition = $keyInlineStyle;
+                $content[] = generateDeepStyleObject(
+                    $block,
+                    $inlineStyle['style'],
+                    $char_position,
+                    $keyInlineStyle,
+                    $breakPoint
+                );
+                $content[] = '';
+                continue 2;
+            }
+        }
+
+        $content[count($content) - 1] .= $character;
+        $char_position++;
+
+        // Closing sub-style
+        foreach (
+            $block['inlineStyleRanges']
+            as $keyInlineStyle => $inlineStyle
+        ) {
+            if ($inlineStyle['offset'] + $inlineStyle['length'] == $key + 1) {
+                $breakPoint[$key + 1] = isset($breakPoint[$key + 1])
+                    ? $breakPoint[$key + 1] + 1
+                    : 1;
+            }
+        }
+    }
+
+    if (empty($content[0])) {
+        array_shift($content);
+    }
+
+    if (empty($content[count($content) - 1])) {
+        unset($content[count($content) - 1]);
+    }
+
+    return [
+        'type' => $style,
+        'content' => $content,
+    ];
+}
---
+++ packages/tools/sugar/src/php/draftJs/renderContent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sugar\draftJs;
+
+/**
+ * @name            renderContent
+ * @namespace       php.draftJs
+ * @type            Function
+ * @platform        php
+ * @status          beta
+ *
+ * Return DraftJs HTML
+ *
+ * @param       {Array}         $deepStyleObject            Deep style object gathered using the `generateDeepStyleObject` function
+ * @param       {Function}      $renderFunction             The render function that will take as parameter an array with the "type" and the "html" properties
+ * @return      {String}                                    Returned HTML
+ *
+ * @since       2.0.0
+ * @author    Paul Balanche <pb@buzzbrothers.ch> (https://buzzbrothers.ch)
+ */
+function renderContent($deepStyleObject, callable $renderFunction)
+{
+    $html = '';
+    if (is_array($deepStyleObject)) {
+        foreach ($deepStyleObject as $subContent) {
+            if (is_array($subContent) && isset($subContent['content'])) {
+                $html .= call_user_func_array($renderFunction, [
+                    [
+                        'type' =>
+                            isset($subContent['type']) &&
+                            $subContent['type'] != null
+                                ? $subContent['type']
+                                : null,
+                        'html' => renderContent(
+                            $subContent['content'],
+                            $renderFunction
+                        ),
+                    ],
+                ]);
+            } else {
+                $html .= $subContent;
+            }
+        }
+    }
+
+    return $html;
+}

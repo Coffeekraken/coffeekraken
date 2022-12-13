aa21d8da660534eda31ef652671240ccfe22efa9
Olivier Bossel <olivier.bossel@gmail.com>
2022-12-13 17:12:31 +0100

    update frontspec typos by adding the "type" property and renaming the passed "type" in the view by "id"

--- packages/themes/s-theme/src/config/themeTypo.config.ts
+++ packages/themes/s-theme/src/config/themeTypo.config.ts
@@ -14,6 +14,7 @@ export function preprocess(api) {
             api.config.themeTypo[name] = {
                 label: __upperFirst(name),
                 group: 'color',
+                type: 'color',
                 style: {
                     color: value,
                 },

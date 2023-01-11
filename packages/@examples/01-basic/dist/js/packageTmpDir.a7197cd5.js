7ab74ce2f4d6a3800e6433c6fbbf3cdb8b8ea04a
Olivier Bossel <olivier.bossel@gmail.com>
2023-01-11 12:25:02 +0100

    up

--- packages/@examples/01-basic/dist/js/packageTmpDir.a7197cd5.js
+++ packages/@examples/01-basic/dist/js/packageTmpDir.a7197cd5.js
@@ -1,4 +1,4 @@
-import { S as SSugarConfig } from "./index.caaeb84d.js";
+import { S as SSugarConfig } from "./index.ad758164.js";
 function __packageTmpDir() {
   return SSugarConfig.get("storage.package.tmpDir");
 }

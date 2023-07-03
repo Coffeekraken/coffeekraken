f0d6e9d7650fb982df3d4b1db96a788a59650560
Olivier Bossel <olivier.bossel@gmail.com>
2023-07-03 15:15:35 +0200

    up

---
+++ packages/@websites/css-coffeekraken-io/.sugar/package.config.ts
@@ -0,0 +1,9 @@
+import { __packageJsonSync } from '@coffeekraken/sugar/package';
+
+export default (api) => {
+    return {
+        get json() {
+            return __packageJsonSync('@coffeekraken/s-postcss-sugar-plugin');
+        },
+    };
+};

176a3fa16fa5a9bb0b476209acd279cd385c49eb
Olivier Bossel <olivier.bossel@gmail.com>
2023-07-06 09:23:56 +0200

    up

--- packages/@websites/css-coffeekraken-io/docker/docker-up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /data/web/coffeekraken/coffeekraken/packages/@websites/css-coffeekraken-io
-npm run static.serve &
-npm run doc.serve
---
+++ packages/@websites/postcss-coffeekraken-io/docker/docker-up.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /data/web/coffeekraken/coffeekraken/packages/@websites/postcss-coffeekraken-io
+npm run static.serve &
+npm run doc.serve
